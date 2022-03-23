882c29002510233ebc17eec38ad43a3f70dbe6cc
Biki Mahamood <92981044+biki404@users.noreply.github.com>
2022-03-23 14:18:00 +0530

    Biki.py

--- Biki.py
+++ Biki.py
@@ -1,13 +1,7 @@
 import os, platform
-try:
-    import requests
-except:
-    os.system('pip2 install requests')
-
-import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from libbiki import main
-    main()
+    from libbiki import main_apv
+    main_apv()
 elif bit == '32bit':
     print "\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools"
